6ca753d35c2d96cfcc8573e935020324aa71aecb
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-05-19 11:24:02 +0530

    NODE INIT

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>LeTS fUCK</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tinder-clone/src/Hearder.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css"
+
+
+function Header(){
+    return{
+        <div className="header">
+            <h2>THis</h2>
+        </div>
+    };
+}
